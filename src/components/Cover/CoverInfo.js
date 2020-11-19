802dbb60fe1c19be446dd414d4af574ac1bea06d
Isa Almena <isaalmena@Gabriels-MBP-2.localdomain>
2020-11-19 19:37:19 +0100

    change object name to camelCase to solve the return object

--- src/components/Cover/CoverInfo.js
+++ src/components/Cover/CoverInfo.js
@@ -3,17 +3,17 @@ import "./cover.css";
 
 
 const songFeeder= [
-  {'Song Name': 'Physical Education', 'Artist Name': 'Animals as Leaders', 'Cover url':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
-  {'Song Name': 'Para Mexer', 'Artist Name': 'Animals as Leaders', 'Cover url':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
-  {'Song Name': 'Street Fighter Mas', 'Artist Name': 'Kamasi Washington', 'Cover url':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
-  {'Song Name': 'Gone', 'Artist Name': 'Anderson Paak', 'Cover url':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
-  {'Song Name': 'Moments', 'Artist Name': 'Kenneth Whalum', 'Cover url':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
-  {'Song Name': 'Esentielles', 'Artist Name': 'Ibrahim Maalouf', 'Cover url':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
-  {'Song Name': 'Nakamarra', 'Artist Name': 'Hiatus Kaiyote', 'Cover url':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
-  {'Song Name': 'Drunk', 'Artist Name': 'Sungazer', 'Cover url':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
-  {'Song Name': 'Boom Bap', 'Artist Name': 'Woo Park', 'Cover url':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
-  {'Song Name': 'Modern Value', 'Artist Name': 'Victoria', 'Cover url':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
-  {'Song Name': 'Tour', 'Artist Name': 'Evan Marien', 'Cover url':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
+  {'songName': 'Physical Education', 'artistName': 'Animals as Leaders', 'coverUrl':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
+  {'songName': 'Para Mexer', 'artistName': 'Animals as Leaders', 'coverUrl':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
+  {'songName': 'Street Fighter Mas', 'artistName': 'Kamasi Washington', 'coverUrl':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
+  {'songName': 'Gone', 'artistName': 'Anderson Paak', 'coverUrl':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
+  {'songName': 'Moments', 'artistName': 'Kenneth Whalum', 'coverUrl':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
+  {'songName': 'Esentielles', 'artistName': 'Ibrahim Maalouf', 'coverUrl':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
+  {'songName': 'Nakamarra', 'artistName': 'Hiatus Kaiyote', 'coverUrl':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
+  {'songName': 'Drunk', 'artistName': 'Sungazer', 'coverUrl':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
+  {'songName': 'Boom Bap', 'artistName': 'Woo Park', 'coverUrl':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
+  {'songName': 'Modern Value', 'artistName': 'Victoria', 'coverUrl':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
+  {'songName': 'Tour', 'artistName': 'Evan Marien', 'coverUrl':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'},
 ]
 
 const artistName='Nombre del artista'
@@ -22,7 +22,7 @@ const likes='Likes'
 export default function CoverInfo() {
   const rand = Math.floor(Math.random()*10)
 
-  const songName= songFeeder[rand]
+  const song= songFeeder[rand]
   return (
     <div>
       <a href='#' className='cover'>
@@ -30,8 +30,9 @@ export default function CoverInfo() {
           src='https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg'
           alt='cover'
         />
-        <p className='songName'>{songName}</p>
+        <p className='songName'>{song.songName}</p>
         <p className='artistName'>{artistName}</p>
+        <p>{song.artistName}</p>
         <div className='numbers'>
           <p className='plays'>{plays}</p>
           <p className='likes'>{likes}</p>
