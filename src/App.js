2120790ca9e8b770e2f8cb1225e87c17ad8db809
Isa Almena <isaalmena@Gabriels-MBP-2.localdomain>
2020-11-23 18:54:44 +0100

    change button register&login with modals, change routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Home from "./pages/home";
-import Register from "./pages/register";
-import Login from "./pages/login";
+import LandingPage from "./pages/LandingPage/LandingPage";
 import Discover from "./pages/discover";
 
 function App() {
@@ -10,21 +8,12 @@ function App() {
     <div>
       <BrowserRouter>
         <Switch>
-          
           <Route exact path='/'>
-             <Home/>
+             <LandingPage/>
           </Route>
-          <Route exact path='/register'>
-            <Register />
-          </Route>
-          <Route exact path='/login'>
-            <Login />
-          </Route>
-          <Route exact path='/Discover'>
+          <Route exact path='/discover'>
             <Discover />
           </Route>
-         
-           
         </Switch>
       </BrowserRouter>
     </div>
---
+++ src/components/Helpers/ServerRequest.js
@@ -0,0 +1,37 @@
+import { getToken } from "../../Utils/LocalStorage.utils";
+
+export const ServerRequest = (routes, method, body) => {
+    const token = getToken();
+    const url = `http://localhost:3000/${routes}`;
+    const JSONBody = JSON.stringify(body);
+    const options = {
+        method: method,
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer' + JSON.parse(token)
+        },
+        body: JSONBody
+    }
+
+    let response
+    return fetch(url, options)
+        .catch(error => (error))
+        .then(res => {
+            if (res.status >= 400) {
+                response = res;
+            }
+            return res.json();
+        })
+        .then(resJson => {
+            return new Promise((resolve, reject) => {
+                if (response) {
+                    reject(resJson);
+                } else {
+                    resolve(resJson)
+                }
+            });
+        });
+
+
+}
