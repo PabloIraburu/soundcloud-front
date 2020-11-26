574fa823da957125cb14508d9a1d93cc95f34b25
Pablo Iraburu <iraburulopez@gmail.com>
2020-11-26 14:49:01 +0100

    Container formatted waiting for general theme

--- src/components/Cover/CoverInfo.js
+++ src/components/Cover/CoverInfo.js
@@ -5,13 +5,13 @@ import "./cover.css";
 const songFeeder= [
   {'Name': 'Physical Education', 'Artist': 'Animals as Leaders', 'URL':'https://upload.wikimedia.org/wikipedia/en/1/1f/The_Joy_Of_Motion2014.jpg', 'likes': '88.000', 'plays': '1M'},
   {'Name': 'Para Mexer', 'Artist': 'Animals as Leaders', 'URL':'https://dummyimage.com/400x400/000/fff', 'likes': '88.000', 'plays': '1M'},
-  {'Name': 'Street Fighter Mas', 'Artist': 'Kamasi Washington', 'URL':'https://avatarfiles.alphacoders.com/782/78200.png', 'likes': '88.000', 'plays': '1M'},
+  {'Name': 'Street Fighter Mas', 'Artist': 'Kamasi Washington', 'URL':'https://i.guim.co.uk/img/media/4c612f741c784ff17a4e363d50906f6ead4b69ec/0_213_5616_3370/master/5616.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=347b58a44be54b67f8fd2b4c118a3646', 'likes': '88.000', 'plays': '1M'},
   {'Name': 'Gone', 'Artist': 'Anderson Paak', 'URL':'https://www.fetchdvm360.com/wp-content/uploads/2018/11/veterinary-behavior-block-400x400.jpg', 'likes': '88.000', 'plays': '1M'},
   {'Name': 'Moments', 'Artist': 'Kenneth Whalum', 'URL':'https://i.pinimg.com/originals/54/6d/41/546d41d7edb34085b69b7c29de441232.jpg', 'likes': '88.000', 'plays': '1M'},
   {'Name': 'Esentielles', 'Artist': 'Ibrahim Maalouf', 'URL':'https://pbs.twimg.com/profile_images/446356636710363136/OYIaJ1KK_400x400.png', 'likes': '88.000', 'plays': '1M'},
   {'Name': 'Nakamarra', 'Artist': 'Hiatus Kaiyote', 'URL':'https://gradle.org/images/gradle-400x400.png', 'likes': '88.000', 'plays': '1M'},
   {'Name': 'Drunk', 'Artist': 'Sungazer', 'URL':'https://jethromanagement.biz/wp-content/uploads/2017/10/instagram-logo-vector-download-400x400.jpg', 'likes': '88.000', 'plays': '1M'},
-  {'Name': 'Boom Bap', 'Artist': 'Woo Park', 'URL':'https://archive-media-0.nyafuu.org/vp/image/1508/39/1508395728106.jpg', 'likes': '88.000', 'plays': '1M'},
+  {'Name': 'Boom Bap', 'Artist': 'Woo Park', 'URL':'https://first-avenue.com/wp-content/uploads/2020/01/woopark.jpg', 'likes': '88.000', 'plays': '1M'},
   {'Name': 'Modern Value', 'Artist': 'Victoria', 'URL':'https://www.mitrefinch.com.au/wp-content/uploads/sites/6/2018/09/piggy-circle-1-400x400.png', 'likes': '88.000', 'plays': '1M'},
   {'Name': 'Tour', 'Artist': 'Evan Marien', 'URL':'https://s3.ap-south-1.amazonaws.com/isupportcause/uploads/overlay/isupportimg_1508181573419.png', 'likes': '88.000', 'plays': '1M'},
 ]
