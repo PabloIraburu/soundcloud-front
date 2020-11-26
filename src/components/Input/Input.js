23466ef498088df69f9a4e114424c1b105b90549
Cris <cristina.picatoste@gmail.com>
2020-11-26 13:45:03 +0100

    Componente input creado y funcionando correctamente

---
+++ src/components/Input/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Input.css";
+
+export const Input = ({type, name, onChange, placeholder}) => {
+  return (
+    <div className="Input-wrap">
+      <input type={type} name={name} onChange={onChange} placeholder={placeholder}/>
+      <icon></icon>
+    </div>
+  )
+}
