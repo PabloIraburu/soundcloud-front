2bead493bd28efeaa6a99540fcb479164c86491a
Cris <cristina.picatoste@gmail.com>
2020-12-04 00:59:23 +0100

    Prototipe landing sin acabar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import LandingPage from "./pages/LandingPage/LandingPage";
-import { DISCOVER, PROFILE, EDITPROFILE, HOME, LIBRARY, UPLOAD } from "./routes/routes";
+import {
+  DISCOVER,
+  PROFILE,
+  EDITPROFILE,
+  HOME,
+  LIBRARY,
+  UPLOAD,
+} from "./routes/routes";
+import { Landing } from "./pages/LandingPage/Landing";
 import { Profile } from "./pages/Profile/Profile";
 import { EditProfile } from "./pages/EditProfile/EditProfile";
 import Discover from "./pages/Discover/NewLanding";
-import { Library } from "./pages/Library/Library"
+import { Library } from "./pages/Library/Library";
 import { Upload } from "./components/Upload/Upload";
-import Navbar from './components/Navbar/Navbar';
-
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   const [isLogged, setIsLogged] = useState(true);
@@ -18,7 +24,7 @@ function App() {
     <Router>
       <div>
         <Switch>
-          <Route exact path={HOME} component={LandingPage} />
+          <Route exact path={HOME} component={Landing} />
           <Route exact path={DISCOVER} component={Discover} />
           <Route exact path={PROFILE} component={Profile} />
           <Route exact path={EDITPROFILE} component={EditProfile} />
---
+++ src/pages/LandingPage/Landing.js
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import styles from "./Landing.module.css";
+import { Modal } from "../../components/Modal/Modal";
+import { MyButton } from "../../components/MyButton/MyButton";
+import { Register } from "../../pages/Register/Register";
+import { Login } from "../../pages/Login/Login";
+import { Upload } from "../../components/Upload/Upload";
+import logo from "../../img/logo.png";
+
+export const Landing = () => {
+  //Gestión modal registro
+  const [openModalRegister, setOpenModalRegister] = useState(false);
+  const handleOpenRegister = () => setOpenModalRegister(!openModalRegister);
+  const handleCloseRegister = (e) => {
+    const { className: el } = e.target;
+    if (el !== "backdrop" && el !== "fas fa-times" && el !== "GoToLogin-link")
+      return;
+    setOpenModalRegister(!openModalRegister);
+  };
+
+  //Gestión modal login
+  const [openModalLogin, setOpenModalLogin] = useState(false);
+  const handleOpenLogin = () => setOpenModalLogin(!openModalLogin);
+  const handleCloseLogin = (e) => {
+    const { className: el } = e.target;
+    if (
+      el !== "backdrop" &&
+      el !== "fas fa-times" &&
+      el !== "GoToRegister-link"
+    )
+      return;
+    setOpenModalLogin(!openModalLogin);
+  };
+
+  //Gestión modal upload
+  const [openModalUpload, setOpenModalUpload] = useState(false);
+  const handleOpenUpload = () => setOpenModalUpload(!openModalUpload);
+  const handleCloseUpload = (e) => {
+    const { className: el } = e.target;
+    if (
+      el !== "backdrop" &&
+      el !== "fas fa-times" &&
+      el !== "GoToRegister-link"
+    )
+      return;
+    setOpenModalUpload(!openModalUpload);
+  };
+
+  return (
+    <div className={styles["Landing"]}>
+      <div className={styles["Landing-menu"]}>
+        <div className={styles["Landing-logo-section"]}>
+          <img src={logo} className={styles["Landing-logo"]} alt="logo" />
+          <h3 className={styles["Landing-logo-text"]}>SoundMist</h3>
+        </div>
+        <div className={styles["Landing-buttons-section"]}>
+          <div className={styles["Landing-buttons"]}>
+            <MyButton onClick={handleOpenLogin} variant="white" size="100px">
+              Sing In
+            </MyButton>
+          </div>
+
+          <div className={styles["Landing-buttons"]}>
+            <MyButton onClick={handleOpenRegister} variant="white" size="150px">
+              Create Account
+            </MyButton>
+          </div>
+
+          <div className={styles["Landing-buttons"]}>
+            <MyButton onClick={handleOpenUpload} variant="pink-or" size="150px">
+              Upload Song
+            </MyButton>
+          </div>
+        </div>
+      </div>
+      <div className={styles["Landing-header"]}>
+        <h1>Listen your music</h1>
+        {/* <div className={styles["Landing-pic-wrap"]}>
+          <img
+            src="https://images.unsplash.com/photo-1520176501380-9a174bf7c783?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80"
+            alt="pic"
+            className={styles["Landing-pic"]}
+          />
+        </div> */}
+      </div>
+
+      {openModalRegister && (
+        <Modal handleClose={handleCloseRegister}>
+          <Register
+            handleCloseRegister={handleCloseRegister}
+            openLogin={handleOpenLogin}
+          />
+        </Modal>
+      )}
+      {openModalLogin && (
+        <Modal handleClose={handleCloseLogin}>
+          <Login
+            handleCloseLogin={handleCloseLogin}
+            openRegister={handleOpenRegister}
+          />
+        </Modal>
+      )}
+      {openModalUpload && (
+        <Modal handleClose={handleCloseUpload}>
+          <Upload />
+        </Modal>
+      )}
+    </div>
+  );
+};
