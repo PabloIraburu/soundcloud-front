eb674475076567a7d345bf3f680bf8e4d176f480
gabviana <gabriela_viana13@hotmail.com>
2021-01-14 16:50:44 +0100

    implementacion rutas privadas

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
 import {
   DISCOVER,
   PROFILE,
@@ -18,9 +18,18 @@ import { Library } from "./pages/Library/Library";
 import { Upload } from "./components/Upload/Upload";
 import {MySongs} from "./pages/MySongs/MySongs";
 import {UserContextProvider} from "./components/UserContext/contextProvider";
+import { getToken } from "./utils/LocalStorage.utils";
+
 
 function App() {
   const [isLogged, setIsLogged] = useState(true);
+
+  const PrivateRoute = ({ component: Component, path }) => (
+    <Route
+      path={path}
+      render={() => getToken() ? <Component /> : <Redirect to={HOME} />}
+    />
+  )
 
   return (
       <Router>
@@ -28,12 +37,12 @@ function App() {
           <Switch>
             <Route exact path={HOME} component={Landing} />
             <UserContextProvider>
-              <Route exact path={DISCOVER} component={Discover} />
-              <Route exact path={PROFILE} component={Profile} />
-              <Route exact path={EDITPROFILE} component={EditProfile} />
-              <Route exact path={LIBRARY} component={Library} />
-              <Route exact path={UPLOAD} component={Upload} />
-              <Route exact path={MYSONGS} component={MySongs} />
+              <PrivateRoute exact path={DISCOVER} component={Discover} />
+              <PrivateRoute exact path={PROFILE} component={Profile} />
+              <PrivateRoute exact path={EDITPROFILE} component={EditProfile} />
+              <PrivateRoute exact path={LIBRARY} component={Library} />
+              <PrivateRoute exact path={UPLOAD} component={Upload} />
+              <PrivateRoute exact path={MYSONGS} component={MySongs} />
             </UserContextProvider>
           </Switch>
         </div>
---
+++ src/routes/PublicRoute.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const PublicRoute = () => {
+    return (
+        <div>
+            <h1>PublicRoute</h1>
+        </div>
+    )
+}
