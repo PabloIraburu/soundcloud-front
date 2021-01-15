a30d27d8c6ce46bacbd94a38cdc033f7b9ca8ee7
Cris <cristina.picatoste@gmail.com>
2021-01-15 19:47:23 +0100

    Maquetación páginas album, playlist, recommended, favouirtes, etc.

--- src/pages/Albums/Albums.js
+++ src/pages/Albums/Albums.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import { CoverBg } from "../../components/CoverBg/CoverBg";
 import { CoverMd } from "../../components/CoverMd/CoverMd";
-import { CoverSm } from "../../components/CoverSm/CoverSm";
 import { ListaSongsFake } from "../../data/fakeSongs";
+import styles from './Albums.module.css';
 
 export const Albums = () => {
 
@@ -10,33 +9,9 @@ export const Albums = () => {
     <>
       <h1>Albums</h1>
 
-      <div className="CoversBg-wrap-profile">
-        {ListaSongsFake.map((song) => (
-          <CoverBg
-            key={song._id}
-            title={song.title}
-            categories={song.categories}
-            author={song.author}
-            img={song.img}
-          />
-        ))}
-      </div>
-
-      <div className="CoversMd-wrap-profile">
+      <div className={styles["Albums-list"]}>
         {ListaSongsFake.map((song) => (
           <CoverMd
-            key={song._id}
-            title={song.title}
-            categories={song.categories}
-            author={song.author}
-            img={song.img}
-          />
-        ))}
-      </div>
-
-      <div className="CoversMd-wrap-profile">
-        {ListaSongsFake.map((song) => (
-          <CoverSm
             key={song._id}
             title={song.title}
             categories={song.categories}
