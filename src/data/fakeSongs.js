001a617a265275214f6b8163e97c3a6adf588898
Cris <cristina.picatoste@gmail.com>
2021-01-15 14:36:52 +0100

    Actualización estilos LateralBar; creación archivo con fakesongs temporal, corrección navegación lateralbar, otros cambios y eliminación de warnings

---
+++ src/data/fakeSongs.js
@@ -0,0 +1,98 @@
+export const ListaSongsFake = [
+  {
+     title: "Soy el 1",
+     categories: "Videojuegos",
+     author: "Autor 1",
+     img:
+       "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80",
+     _id: 1,
+   },
+   {
+     title: "Soy el 2",
+     categories: "Videojuegos",
+     author: "Autor 2",
+     img:
+       "https://images.unsplash.com/photo-1581375074612-d1fd0e661aeb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1502&q=80",
+     _id: 2,
+   },
+   {
+     title: "Soy el 3",
+     categories: "Comedia",
+     author: "Autor 3",
+     img:
+       "https://images.unsplash.com/photo-1584168844383-3c3ee678981a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=682&q=80",
+     _id: 3,
+   },
+   {
+     title: "Soy el 4",
+     categories: "Comedia",
+     author: "Autor 4",
+     img:
+       "https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
+     _id: 4,
+   },
+   {
+     title: "Soy el 5",
+     categories: "Comida",
+     author: "Autor 5",
+     img:
+       "https://images.unsplash.com/photo-1607043832356-06c04346e9c5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+     _id: 5,
+   },
+   {
+     title: "Soy el 6",
+     categories: "Comida",
+     author: "Autor 6",
+     img:
+       "https://images.unsplash.com/photo-1607012104821-b10328a95078?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2763&q=80",
+     _id: 6,
+   },
+   {
+     title: "Soy el 7",
+     categories: "Comedia",
+     author: "Autor 7",
+     img:
+       "https://images.unsplash.com/photo-1607053117298-1e68e05881ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+     _id: 7,
+   },
+   {
+     title: "Soy el 8",
+     categories: "Comedia",
+     author: "Autor 8",
+     img:
+       "https://images.unsplash.com/photo-1550755375-73584cff528c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
+     _id: 8,
+   },
+   {
+     title: "Soy el 9",
+     categories: "Comedia",
+     author: "Autor 9",
+     img:
+       "https://images.unsplash.com/photo-1610644605879-1e0e8fcb9ac2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1402&q=80",
+     _id: 9,
+   },
+   {
+     title: "Soy el 10",
+     categories: "Videojuegos",
+     author: "Autor 10",
+     img:
+       "https://images.unsplash.com/photo-1610643625267-aee6dae3ca22?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80",
+     _id: 10,
+   },
+   {
+     title: "Soy el 11",
+     categories: "Comedia",
+     author: "Autor 11",
+     img:
+       "https://images.unsplash.com/photo-1610568129002-c01dc3cdd39c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80",
+     _id: 11,
+   },
+   {
+     title: "Soy el 12",
+     categories: "Videojuegos",
+     author: "Autor 12",
+     img:
+       "https://images.unsplash.com/photo-1610555422528-07bae5b5cb51?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1234&q=80",
+     _id: 12,
+   }
+]
--- src/pages/Recommended/Recommended.js
+++ src/pages/Recommended/Recommended.js
@@ -2,80 +2,13 @@ import React from 'react';
 import { CoverBg } from "../../components/CoverBg/CoverBg";
 import { CoverMd } from "../../components/CoverMd/CoverMd";
 import { CoverSm } from "../../components/CoverSm/CoverSm";
+import { ListaSongsFake } from "../../data/fakeSongs";
 
 export const Recommended = () => {
 
-  const ListaSongsFake = [
-    {
-      title: "Soy el 1",
-      categories: "Videojuegos",
-      author: "Autor 1",
-      img:
-        "https://images.unsplash.com/photo-1518020382113-a7e8fc38eac9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=720&q=80",
-      _id: 1,
-    },
-    {
-      title: "Soy el 2",
-      categories: "Videojuegos",
-      author: "Autor 2",
-      img:
-        "https://images.unsplash.com/photo-1581375074612-d1fd0e661aeb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1502&q=80",
-      _id: 2,
-    },
-    {
-      title: "Soy el 3",
-      categories: "Comedia",
-      author: "Autor 3",
-      img:
-        "https://images.unsplash.com/photo-1584168844383-3c3ee678981a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=682&q=80",
-      _id: 3,
-    },
-    {
-      title: "Soy el 4",
-      categories: "Comedia",
-      author: "Autor 4",
-      img:
-        "https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80",
-      _id: 4,
-    },
-    {
-      title: "Soy el 5",
-      categories: "Comida",
-      author: "Autor 5",
-      img:
-        "https://images.unsplash.com/photo-1607043832356-06c04346e9c5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
-      _id: 5,
-    },
-    {
-      title: "Soy el 6",
-      categories: "Comida",
-      author: "Autor 6",
-      img:
-        "https://images.unsplash.com/photo-1607012104821-b10328a95078?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2763&q=80",
-      _id: 6,
-    },
-    {
-      title: "Soy el 7",
-      categories: "Comedia",
-      author: "Autor 7",
-      img:
-        "https://images.unsplash.com/photo-1607053117298-1e68e05881ca?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
-      _id: 7,
-    },
-    {
-      title: "Soy el 8",
-      categories: "Comedia",
-      author: "Autor 8",
-      img:
-        "https://images.unsplash.com/photo-1550755375-73584cff528c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
-      _id: 8,
-    },
-  ];
-
-
   return (
     <>
-      <h1></h1>
+      <h1>Recommended</h1>
 
       <div className="CoversBg-wrap-profile">
         {ListaSongsFake.map((song) => (
