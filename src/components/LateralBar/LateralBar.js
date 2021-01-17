dda70b226667270df17e9f5efbbda9b2bd92ceef
Cris <cristina.picatoste@gmail.com>
2021-01-17 15:53:52 +0100

    Revisión componente songs de warning, edición estilo componente CoverSm, eliminación botón updatesong en la barra lateral izquierda y añadido al icono de las gafas de sol

--- src/components/LateralBar/LateralBar.js
+++ src/components/LateralBar/LateralBar.js
@@ -28,7 +28,7 @@ export default function LateralBar() {
     // }
 
     const { signOut } = useContext(UserContext);
-   
+
     //Gestión modal CreatePlaylist
     const [openModalCreatePlaylist, setOpenModalCreatePlaylist] = useState(false);
     const handleOpenCreatePlaylist = () => setOpenModalCreatePlaylist(!openModalCreatePlaylist);
@@ -134,7 +134,7 @@ export default function LateralBar() {
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 576 512"
                             className="svg-inline--fa fa-alien-monster fa-w-18 fa-9x"
-                            
+
                         >
                             <g className="fa-group">
                                 <path
@@ -213,38 +213,6 @@ export default function LateralBar() {
                     </Link>
                 </li>
 
-                {/* UPLOAD BUTTON */}
-                <li className="nav-item">
-                    <p className="nav-link" onClick={handleOpenUpload}>
-                        <svg
-                            aria-hidden="true"
-                            focusable="false"
-                            data-prefix="fad"
-                            data-icon="space-shuttle"
-                            role="img"
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 640 512"
-                            className="svg-inline--fa fa-space-shuttle fa-w-20 fa-5x"
-                        >
-                            <g className="fa-group">
-                                <path
-                                    fill="currentColor"
-                                    d="M32 416c0 35.35 21.49 64 48 64h16V352H32zm154.54-232h280.13L376 168C243 140.59 222.45 51.22 128 34.65V160h18.34a45.62 45.62 0 0 1 40.2 24zM32 96v64h64V32H80c-26.51 0-48 28.65-48 64zm114.34 256H128v125.35C222.45 460.78 243 371.41 376 344l90.67-16H186.54a45.62 45.62 0 0 1-40.2 24z"
-                                    className="fa-secondary"
-                                ></path>
-                                <path
-                                    fill="currentColor"
-                                    d="M592.6 208.24C559.73 192.84 515.78 184 472 184H186.54a45.62 45.62 0 0 0-40.2-24H32c-23.2 0-32 10-32 24v144c0 14 8.82 24 32 24h114.34a45.62 45.62 0 0 0 40.2-24H472c43.78 0 87.73-8.84 120.6-24.24C622.28 289.84 640 272 640 256s-17.72-33.84-47.4-47.76zM488 296a8 8 0 0 1-8-8v-64a8 8 0 0 1 8-8c31.91 0 31.94 80 0 80z"
-                                    className="fa-primary"
-                                ></path>
-                            </g>
-                        </svg>
-                        <MyButton onClick={handleOpenUpload} variant="pink-or" size="150px">
-                            Upload Song
-                        </MyButton>
-                    </p>
-                </li>
-
                 {/* CREATE PLAYLIST */}
                 <li className="nav-item">
                     <p className="nav-link" onClick={handleOpenCreatePlaylist}>
@@ -280,7 +248,7 @@ export default function LateralBar() {
                 {/* BOTTOM ICONS */}
                 <li className="nav-item" id="themeButton">
                     <div className="nav-link">
-                        {/* ICONO 1 */}
+                        {/* ICONO 1 LOGOUT */}
                         <svg
                             className="theme-icon"
                             id="lightIcon"
@@ -309,7 +277,7 @@ export default function LateralBar() {
                             </g>
                         </svg>
 
-                        {/* ICONO 2 */}
+                        {/* ICONO 2 MODAL NEW PLAYLIST */}
                         <svg
                             className="theme-icon"
                             id="solarIcon"
@@ -337,7 +305,7 @@ export default function LateralBar() {
                             </g>
                         </svg>
 
-                        {/* ICONO 3 */}
+                        {/* ICONO 3 UPLOAD SONG */}
                         <svg
                             className="theme-icon"
                             id="darkIcon"
@@ -349,6 +317,7 @@ export default function LateralBar() {
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 576 512"
                             className="svg-inline--fa fa-sunglasses fa-w-18 fa-7x"
+                            onClick={handleOpenUpload}
                         >
                             <g className="fa-group">
                                 <path
