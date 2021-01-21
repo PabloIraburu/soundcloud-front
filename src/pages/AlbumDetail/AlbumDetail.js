dfe13cf0c317ac6aebd36b5964e58816b9c4ecc1
gabviana <gabriela_viana13@hotmail.com>
2021-01-21 20:42:00 +0100

    AlbumDetail done

--- src/pages/AlbumDetail/AlbumDetail.js
+++ src/pages/AlbumDetail/AlbumDetail.js
@@ -1,19 +1,41 @@
 import React, {useContext} from 'react';
-//import { CoverBg } from '../../components/CoverBg/CoverBg';
+import { SongsContext } from '../../contexts/SongsContext/songsContext';
+import { SongItemList } from '../../components/SongItemList/SongItemList';
+import { CoverBg } from '../../components/CoverBg/CoverBg';
 import styles from './AlbumDetail.module.css';
 
 
-export const AlbumDetail = () => {
+export const AlbumDetail = (/*{entity}*/) => {
 
+  const { songs } = useContext(SongsContext);
+  const entity = songs[0];
+  console.log(entity);
 
-
-    return (
-        <>
-            <h1>Album Detail</h1>
-            {/*<p>{songs[0].category}</p>*/}
-            <h1>Popular songs</h1>          
-            
-        </>
-      )
+  return (
+    <>
+    {
+    (entity !== undefined) && 
+    <div className={styles["AlbumDetail-header"]}>
+      <CoverBg
+        title={entity.title}
+        categories={entity.category}
+        author={entity.id_owner}
+        img={entity.image}
+        description={entity.description}
+      />
+    </div>
     }
-    
+
+      {
+      (songs.length !== 0) && 
+        <div className={styles["AlbumDetail-list"]}>
+          {songs.map((songs) => (
+            <SongItemList 
+              song={songs}
+            />
+          ))};
+        </div>
+      }
+    </>
+  )
+}
