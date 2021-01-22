6284076b86c125060c3097c8c1e8347d0ef57fa7
Pablo Iraburu <iraburulopez@gmail.com>
2021-01-22 08:54:03 +0100

    Css User Info

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,35 +1,35 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 import "./UserInfo.css";
 import {UserContext} from "../../contexts/UserContext/contextProvider";
-import { ServerRequest } from '../../helpers/ServerRequest';
+import {ServerRequest} from '../../helpers/ServerRequest';
 
 
 export default function UserInfo() {
-    
-const { user } = useContext(UserContext);
-const [followers, setFollowers] = useState([]);
-console.log(followers);
 
-useEffect(() => {
-    ServerRequest(`data/follower/?followed=${user._id}`, "GET")
-        .then(response => setFollowers(response))
-        .catch(console.log);
-}, [user])
+    const {user} = useContext(UserContext);
+    const [followers, setFollowers] = useState([]);
+    console.log(followers);
 
-    return(
+    useEffect(() => {
+        ServerRequest(`data/follower/?followed=${user._id}`, "GET")
+            .then(response => setFollowers(response))
+            .catch(console.log);
+    }, [user])
+
+    return (
         <a className='userCard' href='/profile'>
-            <div style={{ backgroundImage: `url(${user.image})` }} className="userCard-img"></div>
-            <div className="text">
+            <span className="superior-row">
+                 <div style={{backgroundImage: `url(${user.image})`}} className="userCard-img"></div>
                 <div className="identity">
-                    <h1 className='Name'>{user.name}</h1>
-                </div>
-                <div className="numbers">
                     {followers.length === 0
                         ? <p className="followers">0 Followers</p>
                         : <p className="followers">{followers.length} Followers</p>
                     }
                     <p className="tracks">Tracks</p>
                 </div>
+            </span>
+            <div className="numbers">
+                <h1 className='Name'>{user.name}</h1>
             </div>
         </a>
     )
