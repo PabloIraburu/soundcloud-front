9e9a067c26fbb8e5130d83df543fe746a0b876ca
Cris <cristina.picatoste@gmail.com>
2021-01-23 20:10:47 +0100

    PlayerReducer, playerContext y player component done. Falta hacer pruebas

---
+++ src/reducers/playerReducer.js
@@ -0,0 +1,87 @@
+import { useReducer } from "react";
+
+const initialState = {
+    reproduceSongList: [],
+    currentPlay: undefined
+}
+
+export const playerActions = {
+    //cargar canciones, playlist o album
+    LOAD_SONG: 'LOAD_SONG',
+
+    //añadir canción a la cola de reproducción
+    ADD_TO_QUEUE: 'ADD_TO_QUEUE',
+
+    //iniciar reproducción playlist, album, favoritos
+    START_PLAY: 'START_PLAY',
+    
+    //reproducción ordenada
+    PLAY_REPEAT: 'PLAY_REPEAT',
+
+    //reproducción random
+    PLAY_RANDOM: 'PLAY_RANDOM',
+
+    //reproducir una canción (de otra playlist o de sugerencias)
+    PLAY_SONG: 'PLAY_SONG',
+
+    //reproducir una canción (de otra playlist o de sugerencias)
+    PLAY_THIS_SONG: 'PLAY_THIS_SONG',
+
+    //ir a siguiente canción
+    NEXT_SONG: 'NEXT_SONG',
+
+    //ir a anterior canción
+    PREV_SONG: 'PREV_SONG',
+}
+
+const playerReducer = (state, action) => {
+
+    const newState = { ...state }
+    
+    switch (action.type) {
+        case playerActions.LOAD_SONG:
+            newState.reproduceSongList = action.songs;
+            return newState;
+    
+        case playerActions.ADD_TO_QUEUE:
+            newState.reproduceSongList = [...state.reproduceSongList, action.song];
+            return newState;
+        
+        case playerActions.START_PLAY:
+            newState.currentPlay = 0;
+            return newState;
+        
+        case playerActions.PLAY_REPEAT:
+            return newState;
+
+        case playerActions.PLAY_RANDOM:
+            newState.currentPlay = Math.floor(Math.random() * state.reproduceSongList.length);
+            return newState;
+
+        case playerActions.PLAY_THIS_SONG:
+            newState.currentPlay = action.index;
+            return newState;
+        
+        case playerActions.PLAY_SONG:
+            newState.reproduceSongList = action.song;
+            return newState;
+
+        case playerActions.NEXT_SONG:
+            if (state.reproduceSongList < state.reproduceSongList.length) {
+                newState.currentPlay = state.currentPlay + 1;
+            }
+            return newState;
+
+        case playerActions.PREV_SONG:
+            if (state.reproduceSongList > 0) {
+                newState.currentPlay = state.currentPlay - 1;
+            }
+            return newState;
+
+        default:
+            return state;
+    } 
+}
+
+
+export const usePlayerReducer = () => useReducer(playerReducer, initialState);
--- src/routes/MainRouter.js
+++ src/routes/MainRouter.js
@@ -6,14 +6,17 @@ import LateralBar from "../components/LateralBar/LateralBar";
 import Discover from "../pages/Discover/Discover";
 import { Profile } from "../pages/Profile/Profile";
 import { EditProfile } from "../pages/EditProfile/EditProfile";
-import { Songs } from "../pages/Songs/Songs";
 import { Albums } from "../pages/Albums/Albums";
 import { Playlists } from "../pages/Playlists/Playlists";
 import { Favourites } from "../pages/Favourites/Favourites";
 import { getToken } from "../utils/LocalStorage.utils";
 import { FollowLateralBar } from "../components/FollowLateralBar/FollowLateralBar";
 import { EntityDetail } from "../pages/EntityDetail/EntityDetail";
+import { PlayerContextProvider } from "../contexts/PlayerContext/playerContext";
+import { UserContextProvider } from "../contexts/UserContext/contextProvider";
+import { SongsContextProvider } from "../contexts/SongsContext/songsContext";
 import '../App.css';
+import { Player } from "../components/Player/Player";
 
 
 export const MainRouter = () => {
@@ -25,27 +28,32 @@ export const MainRouter = () => {
     />
   )
 
-
   return (
     <>
-      <LateralBar />
-      <FollowLateralBar />
-      <div className="MainRouter-wrap">
-        <Switch>
-          <PrivateRoute exact path={route.DISCOVER} component={Discover} />
-          <PrivateRoute exact path={route.ALBUMS} component={Albums} />
-          <PrivateRoute exact path={route.PLAYLISTS} component={Playlists} />
-          <PrivateRoute exact path={route.FAVOURITES} component={Favourites} />
-          <PrivateRoute exact path={route.SONGS} component={Songs} />
-          <PrivateRoute exact path={route.PROFILE} component={Profile} />
-          <PrivateRoute exact path={route.EDITPROFILE} component={EditProfile} />
-          <PrivateRoute exact path={route.ALBUMDETAIL} component={EntityDetail} />
-          <PrivateRoute exact path={route.PLAYLISTDETAIL} component={EntityDetail} />
+      <UserContextProvider>
+        <SongsContextProvider>
+          <PlayerContextProvider>
 
-          {/* <Redirect to="/discover" /> */}
+            {/* <Player /> */}
+            <LateralBar />
+            <FollowLateralBar />
+            <div className="MainRouter-wrap">
+              <Switch>
+                <PrivateRoute exact path={route.DISCOVER} component={Discover} />
+                <PrivateRoute exact path={route.ALBUMS} component={Albums} />
+                <PrivateRoute exact path={route.PLAYLISTS} component={Playlists} />
+                <PrivateRoute exact path={route.FAVOURITES} component={Favourites} />
+                <PrivateRoute exact path={route.PROFILE} component={Profile} />
+                <PrivateRoute exact path={route.EDITPROFILE} component={EditProfile} />
+                <PrivateRoute exact path={route.ALBUMDETAIL} component={EntityDetail} />
+                <PrivateRoute exact path={route.PLAYLISTDETAIL} component={EntityDetail} />
+                {/* <Redirect to="/discover" /> */}
+              </Switch>
+            </div>
 
-        </Switch>
-      </div>
+          </PlayerContextProvider>
+        </SongsContextProvider>
+      </UserContextProvider>
     </>
   );
 };
