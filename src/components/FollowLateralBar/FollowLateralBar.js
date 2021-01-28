9666da1031bccc8e6745f8a7142cef3f851cb4a5
Cris <cristina.picatoste@gmail.com>
2021-01-28 23:17:40 +0100

    Corrección error de merge

--- src/components/FollowLateralBar/FollowLateralBar.js
+++ src/components/FollowLateralBar/FollowLateralBar.js
@@ -148,8 +148,6 @@ export const FollowLateralBar = () => {
                         />
                     )}
                 </div>
-             </div>
-
             </div>
         </nav>
     )
